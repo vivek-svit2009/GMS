70f4bbf2a55aca5299681afe3dd6fdb2e023afec
VIVEK KUMAR SHARMA <vivek.svit2009@gmail.com>
2019-11-21 19:54:23 +0530

    login

--- GMS/GMS/Controllers/userController.cs
+++ GMS/GMS/Controllers/userController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,19 +11,58 @@ namespace GMS.Controllers
 {
     public class userController : Controller
     {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
+
         // GET: user
         public ActionResult login()
         {
             return View();
         }
         [HttpPost]
-        public string login(LoginUser form)
+        public ActionResult login(LoginUser form)
         {
-          
-            string UserName = form.email;
-            string Password = form.password;
-           
-            return UserName + Password;
+            SqlCommand cmd = new SqlCommand("Select IsProfile from tbl_User where Email='" + form.email + "' and Password='" + form.password + "' ", con);
+            con.Open();
+            var IsProfile = cmd.ExecuteScalar();
+            con.Close();
+            if (IsProfile != null)
+            {
+                if (IsProfile.ToString() == "True")
+                {
+                    return RedirectToAction("profile", "mentor");
+                }
+                else
+                {
+                    return RedirectToAction("register_info", "mentor");
+                }
+            }
+            else
+            {
+                SqlCommand cmd1 = new SqlCommand("Select IsProfile from tbl_Mentor where Email='" + form.email + "' and Password='" + form.password + "' ", con);
+                con.Open();
+                var IsProfile1 = cmd1.ExecuteScalar();
+                con.Close();
+                if (IsProfile1 != null)
+                {
+
+                    if (IsProfile1.ToString() == "True")
+                    {
+                        return RedirectToAction("profile", "startup");
+                    }
+                    else
+                    {
+                        return RedirectToAction("register_info", "startup");
+                    }
+                }
+                else
+                {
+                    ViewBag.Message = "UserName Or Password is incorrect";
+                    return View();
+                }
+            }
+
+
+            
         }
     }
 }
