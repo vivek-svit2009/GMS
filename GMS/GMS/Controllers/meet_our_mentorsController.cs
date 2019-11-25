855a2d62f533b04cd8806eff3a8eaf682fb6cb31
VIVEK KUMAR SHARMA <vivek.svit2009@gmail.com>
2019-11-25 23:01:25 +0530

    Commit

---
+++ GMS/GMS/Controllers/meet_our_mentorsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GMS.Controllers
+{
+    public class meet_our_mentorsController : Controller
+    {
+        // GET: meet_our_mentors
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
